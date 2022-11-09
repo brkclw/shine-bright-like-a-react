012ca1a7a19b822727f02ff9ca6890d52e840145
brkclw <8033666+brkclw@users.noreply.github.com>
2022-11-09 19:25:18 +0100

    Feature/graphql codegen (#24)
    * chore: add execute permission to build docker script
    
    * feat: generate TS from gql schema
    
    * feat: use auto generate types
    
    * test: add missing variable for gql query

--- src/graphql/queries/repository/repository.query.ts
+++ src/graphql/queries/repository/repository.query.ts
@@ -1,27 +1,22 @@
-import { gql, useQuery } from "@apollo/client";
-
-/** In real word case I would use @graphl-codegen to generate TS types from gql schema */
-export interface Repository {
-  __typename: string;
-  forkCount: number;
-  name: string;
-  stargazerCount: number;
-  url: string;
-}
-
-export interface RepositoriesQueryResponse {
-  search: {
-    __typename: string;
-    repositoryCount: number;
-    nodes: Repository[];
-  };
-}
+import { gql } from "@apollo/client";
 
 export const GET_REPOSITORIES = gql`
-  query GET_REPOSITORIES {
-    search(query: "topic:react stars:>1000", type: REPOSITORY, first: 100) {
+  query GET_REPOSITORIES($first: Int!, $before: String, $after: String) {
+    search(
+      query: "topic:react stars:>1000"
+      type: REPOSITORY
+      first: $first
+      before: $before
+      after: $after
+    ) {
       __typename
       repositoryCount
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        endCursor
+        startCursor
+      }
       nodes {
         ... on Repository {
           __typename
@@ -34,26 +29,3 @@ export const GET_REPOSITORIES = gql`
     }
   }
 `;
-
-/** Had to use GET_REPOSITORIES as GET_TOPICS is returning 502
- *  I have created a ticket for this to github support.
- */
-// const GET_TOPICS = gql`
-//   query topic {
-//     topic(name: "react") {
-//       id
-//       name
-//       repositories(first: 10) {
-//         edges {
-//           node {
-//             id
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
-
-export function useRepositoriesQuery() {
-  return useQuery<RepositoriesQueryResponse>(GET_REPOSITORIES);
-}
