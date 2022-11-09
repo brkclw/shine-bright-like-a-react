897d1386781c2a6df039eea456566a3796d3dc39
brkclw <8033666+brkclw@users.noreply.github.com>
2022-11-09 17:47:27 +0100

    Bugfix/apollo (#23)
    * chore: add execute permission to build docker script
    
    * fix: add ErrorLink for Apollo client
    
    * fix: add missing yarn command for lint

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,13 +1,30 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+
+const errorLink = onError(({ networkError, graphQLErrors }) => {
+  if (graphQLErrors) {
+    console.log(graphQLErrors);
+  }
+  if (networkError) {
+    console.log(networkError);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_API_URL,
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
+}));
 
 export const createApolloClient = () => {
   return new ApolloClient({
-    link: new HttpLink({
-      uri: process.env.REACT_APP_GRAPHQL_API_URL,
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    }),
+    link: from([authLink, httpLink, errorLink]),
     cache: new InMemoryCache(),
   });
 };
