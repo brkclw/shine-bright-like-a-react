d4e9571b23b67018ec65cf828385314084f4ea49
strelamaciej@gmail.com <strelamaciej@gmail.com>
2022-11-14 14:00:29 +0100

    feat: add search component

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { AutoComplete, Tag } from "antd";
+import { StyledForm } from "./Search.styled";
+import { SearchProps } from "./Search.types";
+
+/**
+ * This is really basic form of search:
+ * TODO: some kind of algorithm with rank is needed for showing results
+ * TODO: debounce with callback to set value of search - as for now Apollo is handling previous request canceling
+ */
+export const Search: React.FC<SearchProps> = ({ repositories, onSearch }) => {
+  const handleOnSelect = (value: string) => {
+    if (!value) {
+      return;
+    }
+    onSearch(value);
+  };
+
+  const handleOnSearch = (value: string) => {
+    if (value) {
+      onSearch(value);
+    }
+  };
+
+  const handleOnClear = () => {
+    onSearch("");
+  };
+
+  return (
+    <>
+      <StyledForm layout="inline">
+        <Tag color="blue">topic: react</Tag>
+        <AutoComplete
+          allowClear
+          onClear={handleOnClear}
+          options={repositories.map(({ name }) => ({
+            value: name,
+            label: name,
+          }))}
+          style={{ width: 200 }}
+          onSelect={handleOnSelect}
+          onSearch={handleOnSearch}
+          placeholder="Search GitHub projects"
+        />
+      </StyledForm>
+    </>
+  );
+};
